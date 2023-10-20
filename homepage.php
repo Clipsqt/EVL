0083de19e711a46f03a81d93f047dc8ff6571985
Rodel G. Agocoy <rodelagocoy17@gmail.com>
2023-10-20 11:07:20 +0800

    blahblah

---
+++ homepage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bookman+Old+Style">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="homepage.css">
+    <title>Monitoring Visitor's Logbook Homepage</title>
+</head>
+<body>
+    <header>
+        <img src="depedblurlogo.jpg" alt="Logo">
+    </header>
+
+    <div class="icons-container">
+        <a href="AppointmentForm.php" class="icon">
+            <i class='bx bx-file'></i>
+            <span>Appointment Form</span>
+        </a>
+
+        <a href="AdminLogin.php" class="icon">
+        <i class='bx bx-user'></i>
+            <span>Admin Login</span>
+        </a>
+    </div>
+</body>
+</html>
