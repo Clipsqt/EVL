f6fe2d133e19efc6914f4cc86f79a7749f0f8952
Ivan Policarpio <144110051+Poli010@users.noreply.github.com>
2023-10-27 09:08:04 +0800

    Add files via upload

---
+++ queuing_system.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="queuing_system.css">
+    <title>QUEUING SYSTEM</title>
+</head>
+<body>
+    <div class="datetime">
+        <div class="time"></div>
+        <div class="date"></div>
+    </div>
+
+    <video width="320" height="240" autoplay loop muted>
+        <source src="DEPED HYMN 2023.mp4" type="video/mp4">
+        <source src="DEPED HYMN 2023.webm" type="video/webm">
+        Your browser does not support the video tag.
+    </video>
+    
+    <div class="OSDS">
+        <h3>OSDS</h3>
+        <table>
+            <tr>
+                <th class="name_of_visitors">VISITORS:</th>
+                <th class="proceed">Proceed:</th>
+                <th class="office">OFFICE:</th>
+            </tr>
+                <tr>
+                    <td class="name">Ivan Policarpio</td>
+                    <td>Proceed To:</td>
+                    <td class="department">ICT SERVICES</td>
+                </tr>
+                <tr>
+                    <td class="name">Reymi Angelo Dela Cruz</td>
+                    <td>Proceed To:</td>
+                    <td class="department">PERSONNEL SECTION</td>
+                </tr>
+                <tr>
+                    <td class="name">asd</td>
+                    <td>Proceed To:</td>
+                    <td class="department">RECORD SECTION</td>
+                </tr>
+                <tr>
+                    <td class="name">asd</td>
+                    <td>Proceed To:</td>
+                    <td class="department">PROPERTY AND SUPPLY</td>
+                </tr>
+                <tr>
+                    <td class="name">asd</td>
+                    <td>Proceed To:</td>
+                    <td class="department">PAYROLL SECTION</td>
+                </tr>
+                <tr>
+                    <td class="name">asd</td>
+                    <td>Proceed To:</td>
+                    <td class="department">ACCOUNTING SECTION</td>
+                </tr>
+                <tr>
+                    <td class="name">asd</td>
+                    <td>Proceed To:</td>
+                    <td class="department">BUDGET SECTION</td>
+                </tr>
+                <tr>
+                    <td class="name">asd</td>
+                    <td>Proceed To:</td>
+                    <td class="department">CASH SECTION</td>
+                </tr> 
+        </table> 
+    </div>
+
+
+    <div class="CID">
+        <h3>CID</h3>
+               
+    </div>
+
+    <div class="SGOD">
+        <h3>SGOD</h3>
+        
+    </div>
+
+    <div class="PRIORITY">
+        <h3>PRIORITY</h3>
+    </div>
+
+
+</body>
+<script src="queuing_system.js" defer></script>
+</html>
